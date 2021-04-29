910fb6fe638611b429ab492638e2fd5ec630bfb6
EshaanJoshiSDBI <eshaanjoshi713@gmail.com>
2021-04-29 18:48:31 +0530

    cleaned the main branch

--- Sorting.R
+++
@@ -1,8 +0,0 @@
-stu_name = c("Eshaan","Aditi","Sharvari","Ajitesh","Vansh")
-marks = c(78,69,92,47,68)
-result = data.frame(Name=stu_name, Marks=marks)
-sort(marks)
-order(marks)
-result$City = c("Mumbai","Mumbai","Pune","Mumbai","Pune")
-ind = with(result, order(City,Marks))
-result[ind,]
--- biggestvalueinvector.R
+++
@@ -1,5 +0,0 @@
-#numbers = as.numeric(readline('Enter the numbers'))
-inp1 = readline("Enter the numbers:")
-inp = as.numeric(unlist(strsplit(inp1, ",")))
-inp
-paste('The biggest value in the list of numbers is: ',max(inp))
